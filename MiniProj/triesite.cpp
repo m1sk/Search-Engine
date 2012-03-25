9e4ea07d51c86f64d00d67c39af02072cb0f37b6
Micha Molko <michamolko@gmail.com>
2012-03-25 12:55:03 +0200

    Finished implementing triesite in Stage 1

--- MiniProj/triesite.cpp
+++ MiniProj/triesite.cpp
@@ -1,11 +1,89 @@
 #include "triesite.h"
+#include <exception>
 
-
-triesite::triesite(void)
-{
+triesite::triesite(string name = "", char func = 'c', char mode = 'q') {
+	if((name[1]==':' && name[2]=='\\') || (name.compare("") == 0)) {
+		sitename = name;
+	} else {
+		sitename = getCurrentPath();
+		sitename += ("\\" + name);
+	}
+	if(func == 'C' || func == 'c'){
+	create(sitename);
+	}
+	else if(func == 'M' || func == 'm'){
+	mount(sitename, mode);
+	}
+	if(sitename.compare("") == 0){
+		doclist.clear();
+		mounted = false;
+		mounttype = 0;
+	}
 }
 
+void triesite::create(string path){
+	doclist.clear();
+	int err = createDirectory(path.c_str());
+	if(err != 0){
+		throw new exception("Unable to create a search site");
+	}
+}
 
-triesite::~triesite(void)
-{
+void triesite::mount(string path,char mode='q'){
+	sitename = getFileName(path);
+	list<string> docs = getSubDirectoryNameList(path);
+	list<string>::iterator iter;
+	triedoc* tmp;
+	for(iter = docs.begin(); iter != docs.end(); ++iter){
+		tmp = new triedoc(path, *iter);
+		doclist.push_back(*tmp);
+	}
+	mounttype = mode;
+	mounted = true;
+}
+
+void triesite::unmount(){
+	list<triedoc>::iterator iter;
+	for(iter = doclist.begin(); iter != doclist.end(); ++iter){
+		delete &(*iter);
+	}
+	sitename = "";
+	mounttype = '\0';
+	mounted = false;
+}
+
+triedoc* triesite::docexists(string compname){
+	list<triedoc>::iterator iter;
+	for(iter = doclist.begin(); iter != doclist.end(); ++iter)
+		if(iter->getdocname().compare(compname) == 0){
+			return &(*iter);
+		}
+			
+	return NULL;
+}
+
+void triesite::docupload(string name,char func) {
+	if(docexists(name)==NULL){
+		triedoc doc = triedoc();
+		doc.putdoc(sitename,name,func);
+		doclist.push_back(doc);
+	}
+	else {
+		throw new exception("Document already exists in search site");
+	}
+
+}
+
+string triesite::docdownload(string name,string path = getCurrentPath()) {
+	triedoc* doc = docexists(name);
+	if(doc != NULL){
+		doc->getdoc(sitename,path);
+	}
+	else {
+		throw new exception("Document doesnt exist in search site");
+	}
+}
+
+triesite::~triesite(){
+	unmount();
 }
