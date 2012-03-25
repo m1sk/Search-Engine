e0ed3a8f5d49beae86b5fd96fb43a66a83b9dfef
Micha Molko <michamolko@gmail.com>
2012-03-25 12:55:38 +0200

    Updated the rest of the project

--- MiniProj/trienode.cpp
+++ MiniProj/trienode.cpp
@@ -1,5 +1,5 @@
 #include "trienode.h"
-
+#include <limits>
 
 trienode::trienode(void)
 {
@@ -9,3 +9,20 @@ trienode::trienode(void)
 trienode::~trienode(void)
 {
 }
+
+inline int long_length(){
+	return strlen(itoa(LONG_MAX, NULL, 10));
+}
+
+string trienode::serialize(trienode conv){
+/*	string padded_long = "%0(" + long_length() + ")ld";
+	string serial;
+	sprintf(serial, padded_long + padded_long + padded_long + "%c" + padded_long,
+		conv.nodeserialnr,conv.firstoffset,conv.nrofoccurences,conv.letter,conv.wordend);
+	for(int i = 0; i < LINKS_LENGTH; ++i){
+		serial += conv.links[i];
+	}
+	return serial;*/
+}
+
+trienode trienode::deserialize(string conv){}
